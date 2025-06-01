41067a4b3c93f6e19c6afb37f8ed25940cc83907
EvilColdhand <marco.raditya88@gmail.com>
2025-06-01 22:27:23 +0800

    feat: Add user to run local docker - read SM

--- aws/iam_user.tf
+++ aws/iam_user.tf
@@ -1,3 +1,7 @@
 resource "aws_iam_user" "local_dev_user" {
   name = "fh-local-dev"
 }
+
+resource "aws_iam_access_key" "local_dev_user_key" {
+  user = aws_iam_user.local_dev_user.name
+}
--- aws/outputs.tf
+++ aws/outputs.tf
@@ -14,3 +14,12 @@ output "cluster_id" {
   value = var.enable_eks ? module.eks[0].cluster_id : null
 }
 
+output "aws_access_key_id" {
+  value = aws_iam_access_key.local_dev_user_key.id
+}
+
+output "aws_secret_access_key" {
+  value     = aws_iam_access_key.local_dev_user_key.secret
+  sensitive = true
+}
+
